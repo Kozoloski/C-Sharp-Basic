b8b68dc28258f4d8508235d3d2846d4b5489595a
Kozoloski <107805572+Kozoloski@users.noreply.github.com>
2023-03-09 23:58:26 +0100

    Add files via upload

---
+++ Homework-3-Part1/Homework-3/Task-1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Enter a word:");
+string word = Console.ReadLine();
+
+if (word.Length > 5)
+{
+    string result = word.Substring(word.Length - 5);
+
+    Console.WriteLine("The last 5 characters are: " + result);
+}
+else
+{
+    Console.WriteLine("Invalid Input");
+}
---
+++ Homework-3-Part1/Homework-3/Task-2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Write a short sentence:");
+string shortSentence = Console.ReadLine();
+
+string[] words = shortSentence.Split();
+
+for (int i = 0; i < words.Length; i++)
+{
+    Console.WriteLine(words[i]);
+}
+
