51e6b72859e1d5360817a6903f58db2d297d3407
Kozoloski <107805572+Kozoloski@users.noreply.github.com>
2023-03-10 00:00:15 +0100

    Add files via upload

---
+++ Homework-3-Part2/Homework-3/Task-3/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Enter a number:");
+bool isNumberParsed = int.TryParse(Console.ReadLine(), out int number);
+
+if (isNumberParsed)
+{
+    int getSum(int number)
+    {
+        int sum = 0;
+
+        while (number != 0)
+        {
+            sum = sum + number % 10;
+            number = number / 10;
+        }
+
+        return sum;
+    }
+    Console.Write("The sum of the digits is: " + getSum(number));
+}
+else
+{
+    Console.WriteLine("Invalid Input!");
+}
+
+
---
+++ Homework-3-Part2/Homework-3/Task-4/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine("Enter your date of birth in these possible formats: mm/dd/yyyy, mm.dd.yyyy, or mm dd yyyy");
+string dateOfBirth = Console.ReadLine();
+
+DateTime convertedDateOfBirth = Convert.ToDateTime(dateOfBirth);
+
+DateTime dateTime = DateTime.Now;
+
+int age = dateTime.Year - convertedDateOfBirth.Year;
+
+if (age > 1)
+{
+    if (dateTime.Month < convertedDateOfBirth.Month ||
+    (dateTime.Month == convertedDateOfBirth.Month && dateTime.Day < convertedDateOfBirth.Day)) age--;
+
+    Console.WriteLine($"You are: {age} years old");
+}
+
+if (age == 0)
+{
+    age = dateTime.Month - convertedDateOfBirth.Month;
+
+    age = age <= 0 ? (12 + age) : age;
+
+    age = dateTime.Day - convertedDateOfBirth.Day >= 0 ? age : --age;
+
+    Console.WriteLine($"You are: {age} months old");
+}
+
+if (convertedDateOfBirth > dateTime)
+{
+    Console.WriteLine("Invalid Input");
+}
+
+
+
+
