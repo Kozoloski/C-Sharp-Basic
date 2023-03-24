168fdaf5e3be55500497985e4fb487988d29e01c
Kozoloski <107805572+Kozoloski@users.noreply.github.com>
2023-03-24 14:27:03 +0100

    Add files via upload

---
+++ Homework-7-Part1/Homework-7-Part1/Task-1/Program.cs
@@ -0,0 +1,31 @@
+﻿Queue<int> numbers = new Queue<int>();
+
+while (true)
+{
+    Console.WriteLine("Enter a number:");
+    bool isNumberParsed = int.TryParse(Console.ReadLine(), out int number);
+
+    if (isNumberParsed)
+    {
+        numbers.Enqueue(number);
+
+        Console.WriteLine("Enter a Y to continue, or any other key to exit and print the result:");
+        string input = Console.ReadLine();
+
+        if (input.ToLower() == "Y".ToLower())
+        {
+            continue;
+        }
+
+        else
+        {
+            Console.WriteLine("These are the numbers you have entered in that particular order:");
+            foreach (int item in numbers) Console.WriteLine(item);
+        }
+
+        break;
+    }
+    else Console.WriteLine("You didn't enter a number!");
+}
+
+
---
+++ Homework-7-Part1/Homework-7-Part1/Task-2/Program.cs
@@ -0,0 +1,8 @@
+﻿List<int> listOfNumbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+List<int> squareOfNumbers = listOfNumbers.Select(number => number * number).ToList();
+
+Console.WriteLine("These are the squares of the numbers in the list above:");
+
+foreach (int square in squareOfNumbers) Console.WriteLine(square);
+
+
