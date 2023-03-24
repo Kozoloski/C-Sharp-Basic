54caddc5ef817175cb2635d1bafc67b437409549
Kozoloski <107805572+Kozoloski@users.noreply.github.com>
2023-03-24 14:28:26 +0100

    Add files via upload

---
+++ Homework-7-Part2/Homework-7/Domain/Classes/Animal.cs
@@ -0,0 +1,21 @@
+﻿
+using Domain.Enums;
+
+namespace Domain.Classes
+{
+    public class Animal
+    {
+        public Animal(string name, string color, int age, Gender gender)
+        {
+            Name = name;
+            Color = color;
+            Age = age;
+            Gender = gender;
+        }
+
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public int Age { get; set; }
+        public Gender Gender { get; set;}
+    }
+}
---
+++ Homework-7-Part2/Homework-7/Task-3/Program.cs
@@ -0,0 +1,34 @@
+﻿using Domain.Classes;
+using Domain.Enums;
+using System;
+
+List<Animal> animals = new List<Animal>()
+{
+    new Animal("Lion", "Yellow", 6, Gender.Male),
+    new Animal("Bear", "Brown", 2, Gender.Male),
+    new Animal("Elephant", "Gray", 7, Gender.Female),
+    new Animal("Albatross", "White", 3, Gender.Male),
+    new Animal("Antilope", "Brown", 4, Gender.Male),
+    new Animal("Anaconda", "Yellow", 5, Gender.Male),
+    new Animal("Capybara", "Brown", 7, Gender.Female),
+    new Animal("Hummingbird", "Brown", 7, Gender.Female),
+    new Animal("Caterpillar", "Green", 4, Gender.Female)
+};
+
+
+List<Animal> animalAge = animals.Where(animal => animal.Age >= 5).ToList();
+Console.WriteLine("These animals have 5 or more years:");
+foreach (Animal animal in animalAge) Console.WriteLine(animal.Name);
+
+List<Animal> animalsThatStartWithA = animals.Where(animal => animal.Name.StartsWith("A")).ToList();
+Console.WriteLine("\nThese animals start with A:");
+foreach (Animal animal in animalsThatStartWithA) Console.WriteLine(animal.Name);
+
+List<Animal> animalsThatAreBrownAndMale = animals.Where(animal => animal.Color == "Brown").Where(animal => animal.Gender == Gender.Male).ToList();
+Console.WriteLine("\nThese animals are male and brown:");
+foreach (Animal animal in animalsThatAreBrownAndMale) Console.WriteLine(animal.Name);
+
+Animal firstAnimalWithMoreThanTenCharacters = animals.First(animal => animal.Name.Length >10);
+Console.WriteLine("\nThis is the first animal with more than 10 characters:");
+Console.WriteLine(firstAnimalWithMoreThanTenCharacters.Name);
+
